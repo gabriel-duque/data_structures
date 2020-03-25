5968a90018b7d594568b72f6c6ddceede896cae1
Gabriel Duque <gabriel.duque@lse.epita.fr>
2020-03-26 00:38:08 +0100

    tests: use `set -e` i valgrind testsuite

--- valgrind_testsuite.sh
+++ valgrind_testsuite.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 red='\033[0;31m'
 green='\033[0;32m'
 normal='\033[0m'
@@ -50,9 +52,11 @@ do
         name="$(basename $t)"
         printf "\t%s: " "${name%.c}"
         cp "$t" .
-        gcc -I"$srcdir/$dir" -O0 -g3 "$name"  -fsanitize=address -lasan \
+        gcc -I"$srcdir/$dir" -O0 -g3 "$name" -fsanitize=address -lasan \
             -pthread -L"$srcdir/$dir/.libs" -l$dir -o "${name%.c}"
+        set +e
         output=$(LD_LIBRARY_PATH="$srcdir/$dir/.libs" valgrind --leak-check=full ./"${name%.c}" 2>&1 | grep 'lost: [0-9]*[,]*[0-9]* bytes in [0-9]* blocks$')"\n"
+        set -e
         printf "$output" | if grep -q 'lost: [0-9]*[,][0-9]* bytes in [^0][0-9]* blocks'; then printf "${red}failed${normal}\n" && printf "$output" && ret_value=1 ; else printf "${green}success${normal}\n" ;fi
     done
 done
